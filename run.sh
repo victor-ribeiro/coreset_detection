bca628807d577bb30220adbaff0110b25d05c8c7
victor-ribeiro <ribeiro.victor.dornellas@gmail.com>
2025-05-12 14:37:19 -0300

    time measuer

--- run.sh
+++ run.sh
@@ -6,14 +6,14 @@
 dataset=covtype
 model=XGBClassifier
 
-for frac in .01 .02 .03 .04 .05 .1 .2 .3 .04;
+for frac in .01 .02 .03 .04 .05 .1 .2 .3 .4 .5;
 do
-    # for method in pmi_kmeans freddy random craig;
-    for method in freddy;
+    # for method in freddy gradmatch random;
+    for method in freddy random gradmatch;
     do 
         python main.py --dataset $dataset --method $method  --model $model --run 10 --tol .001 --resample 5 --train_frac $frac --batch_size 500 --alpha 1
     done
 done
 
 
-# python main.py --dataset $dataset --method none  --model $model --run 10 --resample 5 --train_frac 1
+python main.py --dataset $dataset --method none  --model $model --run 10 --resample 5 --train_frac 1
--- sampling/lazzy_greddy.py
+++ sampling/lazzy_greddy.py
@@ -117,6 +117,7 @@ def freddy(
     for ds in batched(dataset, batch_size):
         ds = np.array(ds)
         base_inc = _base_inc(alpha)
+        # D = pairwise_distances(ds, metric="l1")
         D = pairwise_distances(ds)
         D = D.max() - D  # * (entropy(dataset) - entropy(dataset[sset]))
         # size = len(D)
@@ -129,7 +130,8 @@ def freddy(
             score, idx_s = q.head
             s = D[idx_s[1]]
             score_s = utility_score(s, localmax, acc=argmax, alpha=alpha, beta=beta)
-            score_s *= h_ - entropy(ds[idx_s[1]]) + entropy(ds[_sset])
+            score_s *= h_ - entropy(dataset[sset + [idx_s[1]]])
+
             inc = score_s - score
             if (inc < 0) or (not q):
                 # break
